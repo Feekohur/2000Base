a3aae8393ddec9ebd750d8bf18f77f03fffb22fd
Michał Król <77741@student.pb.edu.pl>
2025-11-16 23:58:19 +0100

    Initial commit

---
+++ 2000BaseNET/Controllers/GameController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2000BaseNET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+    }
+}
---
+++ 2000BaseNET/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿namespace _2000BaseNET.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string? Event { get; set; }
+        public string? Site { get; set; }
+        public string? Date { get; set; }
+        public string? Round { get; set; }
+        public string? White { get; set; }
+        public string? Black { get; set; }
+        public string? Result { get; set; }
+        public string? Moves { get; set; }
+
+    }
+}
